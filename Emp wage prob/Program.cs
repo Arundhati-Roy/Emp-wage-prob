55f8782bff2e4d2d6cedd782028f3a175131f798
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-09-28 14:45:30 +0530

    [AR] Refractor. Use case 7 - Using 2 classes Added

--- Emp wage prob/Program.cs
+++ Emp wage prob/Program.cs
@@ -10,33 +10,38 @@ namespace Coding_prac
         public const int emp_rate_per_hr = 20;
         public const int no_work_days = 2;
         public const int max_hrs_in_mon = 10;
-
+        public static int pacheck(int r)
+        {
+            int emphrs = 0;
+            switch (r)
+            {
+                case is_part_time:
+                    emphrs = 4;
+                    break;
+                case is_full_time:
+                    emphrs = 8;
+                    break;
+                default:
+                    emphrs = 0;
+                    break;
+            }
+            return emphrs;
+        }
         public static int WageCal()
         {
             ///Variables
-            int emphrs = 0;
+            int emphr = 0;
             int totalemphrs = 0;
             int totalworkdays = 0;
             while (totalemphrs <= max_hrs_in_mon && totalworkdays < no_work_days)
             {
                 totalworkdays++;
                 Random random = new Random();
-                int empcheck = random.Next(0, 3);
-
-                switch (empcheck)
-                {
-                    case is_part_time:
-                        emphrs = 4;
-                        break;
-                    case is_full_time:
-                        emphrs = 8;
-                        break;
-                    default:
-                        emphrs = 0;
-                        break;
-                }
-                totalemphrs += emphrs;
-                Console.WriteLine("Days:" + totalworkdays + "Emp Hrs:" + emphrs);
+                int r = random.Next(0, 3);
+                emphr = pacheck(r);
+                
+                totalemphrs += emphr;
+                Console.WriteLine("Days:" + totalworkdays + "Emp Hrs:" + emphr);
             }
             int totalempwage = totalemphrs * emp_rate_per_hr;
             return totalempwage;
