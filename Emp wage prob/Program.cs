048086aa3300c09fe399b8809e97754d4a7c2de1
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-09-28 11:50:11 +0530

    [AR] First commit

---
+++ Emp wage prob/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Emp_wage_prob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage problem!");
+        }
+    }
+}
