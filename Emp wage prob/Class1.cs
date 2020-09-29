7141ec6eee60512ce3b685f1ff49abc88492784e
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-09-29 13:12:11 +0530

    [AR] Refractor. Use case 10 - Manage multiple companies using class and list Added

---
+++ Emp wage prob/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coding_prac
+{
+    //public class Company
+    //{
+    //    public int emp_rate_per_hr = 20;
+    //    public int no_work_days = 2;
+    //    public int max_hrs_in_mon = 10;
+    //    public string compName;
+
+    //    public Company(string compName, int emp_rate_per_hr, int no_work_days, int max_hrs_in_mon)
+    //    {
+    //        this.compName = compName;
+    //        this.emp_rate_per_hr = emp_rate_per_hr;
+    //        this.no_work_days = no_work_days;
+    //        this.max_hrs_in_mon = max_hrs_in_mon;
+    //    }
+    //}
+}
--- Emp wage prob/Program.cs
+++ Emp wage prob/Program.cs
@@ -1,9 +1,23 @@
 ﻿///using com.sun.org.apache.bcel.@internal.generic;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace Coding_prac
 {
+    public class Company
+    {
+        public string compName { get; set; }
+        public int emp_rate_perHr { get; set; }
+        public int no_work_days { get; set; }
+        public int max_hrs_in_mon { get; set; }
+        public int cal_wage { get; set; }
+        //string compName, int emp_rate_perHr, int no_work_days, int max_hrs_in_mon, int cal_wage
+        public string CompanyDet()
+        {
+            return string.Format("{0} is an author of {1}. Price: ${2}",compName,emp_rate_perHr,no_work_days,max_hrs_in_mon,cal_wage);
+        }
+    }
     class Program
     {
         ///Constraints
@@ -16,7 +30,7 @@ namespace Coding_prac
         static void Main(string[] args)
         {
             Console.WriteLine("Employee Wage Prob");
-            List<String> Emp = new List<String>();
+            List<Company> Emp = new List<Company>();
             Console.WriteLine("Enter no of companies: ");
             int n = Convert.ToInt32(Console.ReadLine());
             for (int i = 1; i <= n; i++)
@@ -24,23 +38,31 @@ namespace Coding_prac
                 List<String> b = new List<String>();
                 Console.WriteLine("Comp Name");
                 String compName1 = Console.ReadLine();
-                b.Add(compName1);
+                //b.Add(compName1);
                 Console.WriteLine("Emp Rate");
                 String emp_rate_per_hr = Console.ReadLine();
-                b.Add(emp_rate_per_hr);
+                //b.Add(emp_rate_per_hr);
                 Console.WriteLine("No. of working hrs");
                 String no_of_work_days = Console.ReadLine();
-                b.Add(no_of_work_days);
+                //b.Add(no_of_work_days);
                 Console.WriteLine("Max work hrs in a month");
                 String max_hrs_in_mon = Console.ReadLine();
-                b.Add(max_hrs_in_mon);
+                //b.Add(max_hrs_in_mon);
                 int cw1 = CalEmpWage(compName1, Convert.ToInt32(emp_rate_per_hr),Convert.ToInt32(no_of_work_days),Convert.ToInt32(max_hrs_in_mon));
                 Console.WriteLine("Total wage for " + compName1 + " is " + cw1);
-                b.Add(cw1.ToString());
-                Emp.AddRange(b);
+                //b.Add(cw1.ToString());
+                //new Company { Name = "Mahesh Chand", Book = "ADO.NET Programming", Price = 49.95 }; 
+                Emp.Add( new Company { compName = compName1, emp_rate_perHr = Convert.ToInt32(emp_rate_per_hr), no_work_days = Convert.ToInt32(no_of_work_days), max_hrs_in_mon = Convert.ToInt32(max_hrs_in_mon), cal_wage = cw1 });
+                //Emp.Add(new company);
+                //Emp.AddRange(b);
             }
-            Emp.ForEach(Console.WriteLine);
+            foreach (Company i in Emp)
+            {
+                Console.WriteLine($"Company Details: {i.compName}:{i.emp_rate_perHr}:{i.no_work_days}:{i.max_hrs_in_mon}:{i.cal_wage}");
+            }
         }
+
+        
 
         public static int CalEmpWage(String compName, int emp_rate_per_hr, int no_work_days, int max_hrs_in_mon)
         {
@@ -76,5 +98,5 @@ namespace Coding_prac
 
 
     }
-
+    
 }
